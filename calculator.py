2a9bb11a03bd7cce6c0c071cad0d1c8b27d7a97e
jyotiraut123 <149391422+jyotiraut123@users.noreply.github.com>
2023-10-30 18:38:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    ans=a+b
+    print("Addition=",ans)
+def sub(a,b):
+    ans=a-b
+    print("Sub=",ans)
+def mul(a,b):
+    ans=a*b
+    print("mul=",ans)
+def div(a,b):
+    ans=(a/b)
+    print("Div=",ans)
+
+
+add(10,20)
+sub(20,10)
+mul(2,3)
+div(3,7)
